05cb7626605d0cfe92194a7a1ea1f0249f3ee43f
Valekantina <vgordynska@gmail.com>
2023-04-12 12:03:39 +0200

    added files

---
+++ src/crud.py
@@ -0,0 +1,52 @@
+from src.db import session
+
+
+def create_data(model, name):
+    data = model(name=name)
+    session.add(data)
+    session.commit()
+    print(f"Added: {data.name}")
+    session.close()
+
+
+def list_data(model):
+    data = session.query(model).all()
+    print("List of data:")
+    for s in data:
+        print(s.id, s.name)
+    return data
+
+
+def update_data(model, id_, name):
+    try:
+        new_st = session.query(model).filter(model.id == id_).first()
+        if new_st:
+            new_st.fullname = name
+            upd_st = session.merge(new_st)
+            session.commit()
+            print(f'Changed data {upd_st.id, upd_st.name}')
+            session.close()
+            return upd_st
+        return None
+    except Exception as e:
+        print(f'Error: {e}')
+        session.rollback()
+        session.close()
+        return None
+
+
+def remove_data(model, id_):
+    try:
+        remove_st = session.query(model).filter(model.id == id_).first()
+        if remove_st:
+            print(f'Deleted data: \n{remove_st.id} {remove_st.fullname}')
+            r = session.query(model).filter(model.id == id_).delete()
+            session.commit()
+            session.close()
+            return r
+        return None
+    except Exception as e:
+        print(f'Error: {e}')
+        session.rollback()
+        session.close()
+        return None
